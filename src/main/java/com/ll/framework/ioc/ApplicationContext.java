e9adf5cf7e5f1e7ed8c485eef6d1ef53909e39d2
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-08-19 12:16:58 +0900

    [feat] class를 스캔하고, class가 이미 hashmap에 존재하면 그대로 반환, 아닐 경우 생성

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,19 +1,44 @@
 package com.ll.framework.ioc;
 
+import com.ll.framework.ioc.annotations.Service;
 import org.reflections.Reflections;
+
+import java.beans.Introspector;
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ApplicationContext {
     private final String basePackage;
+    private final Map<String, Class<?>> providers = new HashMap<>();
+    private final Map<String, Object> beans = new HashMap<>();
+
     public ApplicationContext(String basePackage) {
         this.basePackage = basePackage;
     }
 
     public void init() {
         Reflections reflections = new Reflections(basePackage);
+        for (Class<?> clazz : reflections.getTypesAnnotatedWith(Service.class)) {
+            String name = Introspector.decapitalize(clazz.getSimpleName());
+            providers.put(name, clazz);
+        }
     }
 
     public <T> T genBean(String beanName) {
-        
+        Object bean = beans.get(beanName);
+        if (bean != null) return (T) bean;
+        Class<?> clazz = providers.get(beanName);
+        if (clazz == null) return null;
 
+        try {
+            Constructor<?> constructor = clazz.getDeclaredConstructors()[0];
+            constructor.setAccessible(true);
+
+            Object[] args = new Object[constructor.getParameterCount()];
+            Object instance = constructor.newInstance(args);
+            beans.put(beanName, instance);
+            return (T) instance;
+        } catch (Exception e) {return null;}
     }
 }
